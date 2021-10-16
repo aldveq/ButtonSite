7ea4d48d86b6bf170fe18411e0e25ea6f5cc86b7
Aldo Paz Velasquez <aldveq80@gmail.com>
2021-10-16 16:16:11 -0600

    Button finished

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+(
+    function autoExecute() {
+        document.getElementById('buttonBrown').addEventListener('click', () => {
+            alert('Welcome Yader, Hiuberts & Yasir! You will become a WordPress Developer!');
+        })
+    }
+)();
