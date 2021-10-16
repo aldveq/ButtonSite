66745172328c4741d57d182d8e1bd673b255da2d
Hiuberts Duarte <hiduarte@gbm.net>
2021-10-16 16:37:49 -0600

    Creacion de boton con alerta

---
+++ assets/js/hiubert-js.js
@@ -0,0 +1,13 @@
+    
+
+function Alerta() {
+      
+    var mensaje = confirm("Hola, Mi nombre es hiuberst!");
+          
+    if (mensaje) {
+        alert("Funciona");
+    }
+    else {
+        alert("=(");
+    }
+}
